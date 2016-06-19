a965897ed82bc806a83ea99e74c51926cf555770
dhayanithi1987 <dhaya@twoboxventures.com>
2016-06-19 10:03:43 +0530

    Initial Commit

---
+++ NumericLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NumericLock
+//
+//  Created by Dhayanithi P on 19/06/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
